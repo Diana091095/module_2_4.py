313694f5cbcd4b725b2a9a71c1c9d2ba2dddc8b1
Diana091095 <diana091095@mail.ru>
2024-07-04 16:08:29 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,34 +1,21 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-for i in range(len(numbers)):
-     print(numbers[i])
-     x = numbers[i]
-     if x > 1:
-         for i in range(2, (x // 2) + 1):
-             if x % i == 0:
-                 print("число не простое")
-                 break
-         else:
-             print("число простое")
-     else:
-         print("число не простое")
-
-primes = [2, 3, 5, 7, 11, 13]
-print('primes ', primes)
-not_primes = [4, 6, 8, 9, 10, 12, 14, 15]
-print('not_primes ', not_primes)
+primes = []
+not_primes = []
+for i in numbers:
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime == True:
+        primes.append(i)
+    if is_prime == False:
+        not_primes.append(i)
 
 
-# ПОЖАЛУЙСТА, СМОТРИТЕ ВЫШЕ.КОД НИЖЕ ОСТАВЛЕН КАК НАПОМИНАНИЕ. БЛАГОДАРЮ.
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-#
-# for i in range(len(numbers)):
-#     print(numbers[i])
-#     x = numbers[i]
-#     for i in range(2, x):
-#         if x % i == 0:
-#             print('число не простое')
-#         else:
-#             print('число простое')
+print('primes: ', primes)
+print('not_primes: ', not_primes)
+
 
 
 
